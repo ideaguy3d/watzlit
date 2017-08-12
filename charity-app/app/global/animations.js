e2d7bd5c4a9336bdcbb476ce2f141827f5c0e52f
Julius Alvarado <angularjs.uiux@gmail.com>
2017-08-12 01:51:42 -0700

    added gsap anims to following directives

--- charity-app/app/global/animations.js
+++ charity-app/app/global/animations.js
@@ -6,7 +6,7 @@
 
     angular.module('app.global').animation('.j-animate-fade-in-out', ['$window',
         function ($window) {
-            function animate(element, property, start, end, time, units, callback) {
+            /*function animate(element, property, start, end, time, units, callback) {
                 units = units || '';
                 var frameRate = 1/16;
                 var delta = (end - start) / time / frameRate;
@@ -25,14 +25,37 @@
                     value += delta;
                     element.style[property] = value + units;
                 }
-            } // END OF: animate()
-
+            }*/ // END OF: animate()
+            var duration = 1;
             return {
                 enter: function (element, done) {
-                    animate(element[0], 'opacity', 0, 1, 1000, null, done);
+                    // animate(element[0], 'opacity', 0, 1, 1000, null, done);
+                    TweenMax.from(element, duration, {
+                       opacity: 0,
+                        x: '-100%',
+                        ease: Back.easeOut,
+                        onComplete: done
+                    });
                 },
                 leave: function (element, done) {
-                    animate(element[0], 'opacity', 1, 0, 1000, null, done);
+                    // animate(element[0], 'opacity', 1, 0, 1000, null, done);
+                    TweenMax.to(element, duration, {
+                        opacity: 0,
+                        x: '-100%',
+                        ease: Back.easeIn,
+                        onComplete: done
+                    });
+
+                    var width = element[0].offsetWidth;
+                    var delay = duration * .3;
+                    console.log("element[0] = ");
+                    console.log(element[0]);
+                    TweenMax.to(element, duration-delay, {
+                        marginRight: -width,
+                        delay: delay,
+                        ease: Power4.easeIn,
+                        onComplete: done
+                    });
                 }
             }
         }]);

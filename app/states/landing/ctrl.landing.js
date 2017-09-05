1427215ae2e275f0e2bebbf23381a8cc0e061423
Julius Alvarado <angularjs.uiux@gmail.com>
2017-09-04 21:20:09 -0700

    got a much prettier hero section of landing page, has cute mascot now

---
+++ app/states/landing/ctrl.landing.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Julius Alvarado on 9/4/2017.
+ */
+
+(function(){
+    "use strict";
+
+    angular.module('edhubJobsApp').controller('LandingCtrl', [
+        function(){
+            const vm = this;
+            vm.imagePath = 'styles/img'
+        }
+    ])
+}());

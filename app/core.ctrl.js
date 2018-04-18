ae2415ce07df39b62d8b8fc2accb03ff361c9692
Julius Alvarado <angularjs.uiux@gmail.com>
2018-04-17 19:24:21 -0700

    got a very beginner level sidenav toggle to work for the landing page

--- app/core.ctrl.js
+++ app/core.ctrl.js
@@ -2,17 +2,25 @@
  * Created by Julius Alvarado on 9/10/2017.
  */
 
-
 (function () {
     "use strict";
 
-    angular.module('edhubJobsApp').controller('CoreCtrl', ['$scope',
-        function ($scope) {
+    angular.module('edhubJobsApp').controller('CoreCtrl', ['$scope', '$mdSidenav',
+        function ($scope, $mdSidenav) {
             $scope.ccCurrentUser = "";
             $scope.coreEdhubHorizontalState = false;
 
-            $scope.ccSetCurrentUser = function(userEmail){
+            $scope.ccSetCurrentUser = function (userEmail) {
                 $scope.ccCurrentUser = userEmail;
+            }
+
+            $scope.coreEdhubToggleSideNav = coreEdhubToggleSideNav('core-sidenav');
+
+            function coreEdhubToggleSideNav (componentId) {
+
+                return function(){
+                    $mdSidenav(componentId).toggle();
+                }
             }
         }
     ]);

390fc693eafde99fce42d48b224228f3336e1bfe
Julius Alvarado <angularjs.uiux@gmail.com>
2018-04-26 15:34:23 -0700

    REBUILT initial auth factory

--- app/core.ctrl.js
+++ app/core.ctrl.js
@@ -5,9 +5,12 @@
 (function () {
     "use strict";
 
-    angular.module('edhubJobsApp').controller('CoreCtrl', ['$scope', '$mdSidenav', '$mdDialog', '$timeout', CoreClass]);
+    angular.module('edhubJobsApp').controller('CoreCtrl', ['$scope', '$mdSidenav',
+        '$mdDialog', '$timeout', 'edhubAuthService', '$location',
+        CoreClass
+    ]);
 
-    function CoreClass($scope, $mdSidenav, $mdDialog, $timeout) {
+    function CoreClass($scope, $mdSidenav, $mdDialog, $timeout, edhubAuthService, $location) {
         $scope.ccCurrentUser = "";
         $scope.coreEdhubHorizontalState = false;
         $scope.ccAuthBoxHidden = false;
@@ -15,13 +18,19 @@
         $scope.ccAuthBoxHover = true;
         $scope.coreEdhubToggleSideNav = coreEdhubToggleSideNav('core-sidenav');
 
+        const enumAuthBox = {
+            loginSignup: "Login/Signup",
+            settings: "Settings",
+            editProfile: "Edit Profile"
+        };
+
         $scope.ccSetCurrentUser = function (userEmail) {
             $scope.ccCurrentUser = userEmail;
         };
 
-        function coreEdhubToggleSideNav (componentId) {
+        function coreEdhubToggleSideNav(componentId) {
             console.log("edhub - coreEdhubToggleSideNav() invoked");
-            return function(){
+            return function () {
                 $mdSidenav(componentId).toggle();
             }
         }
@@ -30,9 +39,9 @@
         // after the speed dial has opened
         // so that they have the proper position; if closing,
         // immediately hide the tooltips
-        $scope.$watch('ccAuthBoxIsOpen', function(isOpen) {
+        $scope.$watch('ccAuthBoxIsOpen', function (isOpen) {
             if (isOpen) {
-                $timeout(function() {
+                $timeout(function () {
                     $scope.tooltipVisible = $scope.ccAuthBoxIsOpen;
                 }, 400);
             } else {
@@ -40,32 +49,51 @@
             }
         });
 
+        // for ng-md-icon
         $scope.ccItems = [
-            { name: "Login/Signup", icon: "login", direction: "bottom" },
-            { name: "Edit Profile", icon: "edit", direction: "top" },
-            { name: "Settings", icon: "settings", direction: "bottom" }
+            {name: enumAuthBox.loginSignup, icon: "login", direction: "bottom"},
+            {name: enumAuthBox.editProfile, icon: "edit", direction: "top"},
+            {name: enumAuthBox.settings, icon: "settings", direction: "bottom"}
         ];
 
+        // for md-icon
         $scope.ccCustomIcons = [
-            { name: "Login", icon: "img/icons/twitter.svg", direction: "bottom" },
-            { name: "Edit Profile", icon: "img/icons/facebook.svg", direction: "top" },
-            { name: "Settings", icon: "img/icons/hangout.svg", direction: "bottom" }
+            {name: "Login", icon: "img/icons/twitter.svg", direction: "bottom"},
+            {name: "Edit Profile", icon: "img/icons/facebook.svg", direction: "top"},
+            {name: "Settings", icon: "img/icons/hangout.svg", direction: "bottom"}
         ];
 
+        $scope.ccAuthBoxAction = function ($event, item) {
+            switch (item.name) {
+                case enumAuthBox.loginSignup:
+                    _loginSignup();
+                    break;
+                case enumAuthBox.settings:
+                    _settings();
+                    break;
+                case enumAuthBox.editProfile:
+                    _editProfile();
+                    break;
+                default:
+                    console.error("Something went wront w/the AuthBox actions");
+            }
+        };
+
+        // as of 12:08pm-4/26/2018, the modal doesn't center correctly :(
         // TODO: eventually get this modal to work because it's really REALLY cool!
-        $scope.ccOpenDialog = function($event, item) {
+        $scope.ccOpenDialog = function ($event, item) {
             // Show the dialog
             $mdDialog.show({
                 clickOutsideToClose: true,
-                controller: function($mdDialog) {
+                controller: function ($mdDialog) {
                     // Save the clicked item
                     this.item = item;
 
                     // Setup some handlers
-                    this.close = function() {
+                    this.close = function () {
                         $mdDialog.cancel();
                     };
-                    this.submit = function() {
+                    this.submit = function () {
                         $mdDialog.hide();
                     };
                 },
@@ -74,5 +102,17 @@
                 targetEvent: $event
             });
         };
+
+        function _loginSignup() {
+            console.log("in _authAction() !! ^_^");
+        }
+
+        function _settings() {
+            console.log("in _settings() [=");
+        }
+
+        function _editProfile() {
+            console.log("in _editProfile() ! :)");
+        }
     }
 }());
--- app/services/ser.auth.js
+++ app/services/ser.auth.js
@@ -5,22 +5,69 @@
 (function () {
     "use strict";
 
-    angular.module('edhubJobsApp').factory('edhubAuthService', ['$firebaseAuth',
-        function ($firebaseAuth) {
-            const userSignup = function (email, pw) {
-                return $firebaseAuth().$createUserWithEmailAndPassword(email, pw);
-            };
-
-            const userLogin = function (email, pw) {
-                return $firebaseAuth().$signInWithEmailAndPassword(email, pw);
-            };
-
-            // return $firebaseAuth();
-            return {
-                firebase: $firebaseAuth(),
-                userSignup: userSignup,
-                userLogin: userLogin
-            };
-        }
+    angular.module('edhubJobsApp').factory('edhubAuthService', ['$firebaseAuth', '$rootScope',
+        '$firebaseObject', '$location',
+        EdhubAuthClass
     ]);
+
+    function EdhubAuthClass($firebaseAuth, $rootScope, $firebaseObject, $location) {
+        $rootScope.edhubAuthUser = "";
+        const orgRef = firebase.database().ref('organizations');
+        const auth = $firebaseAuth();
+        let authApi = {};
+
+        auth.$onAuthStateChanged(function (authUser) {
+
+        });
+
+        authApi = {
+            login: function (user) {
+                auth.$signInWithEmailAndPassword(user.email, user.password)
+                    .then(function (user) {
+                        console.log("edhub - user successfully signed in");
+                        console.log(user);
+                        $location.path('/');
+                    })
+                    .catch(function (error) {
+                        console.error("edhub - There was an error:")
+                        console.log(error);
+                        $rootScope.authError = error.message;
+                    });
+            },
+            logout: function () {
+                return auth.$signOut();
+            },
+            requireAuth: function () {
+                return auth.requireSignIn();
+            },
+            signup: function (user) {
+                auth.$createUserWithEmailAndPassword(user.email, user.password)
+                    .then(function(regUser){
+                        orgRef.child(regUser.uid).set({
+                            date: firebase.database.ServerValue.TIMESTAMP,
+                            regUser: regUser.uid,
+                            orgName: user.orgName,
+                            email: user.email
+                        });
+                        $rootScope.message = "Thanks for registering " + user.name;
+                        authApi.login(user);
+                    })
+                    .catch(function(error){
+                        console.error("edhub - There was an error =");
+                        console.log(error);
+                    });
+            }
+        };
+
+        const userSignup = function (email, pw) {
+            return $firebaseAuth().$createUserWithEmailAndPassword(email, pw);
+        };
+
+        const userLogin = function (email, pw) {
+            return $firebaseAuth().$signInWithEmailAndPassword(email, pw);
+        };
+
+        // return $firebaseAuth();
+        return authApi;
+    }
 }());

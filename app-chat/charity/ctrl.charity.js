2e475eee4165cf519b4afbd0624a9c24627f1f39
Julius Alvarado <javascript.uiux@gmail.com>
2019-02-17 03:48:54 -0800

    deleted apps that were still using ui-router

--- app-chat/charity/ctrl.charity.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Julius Alvarado on 8/9/2017.
- */
-
-
-angular.module('app').controller('CharityCtrl', function(){
-
-});
---
+++ app/states/chat/ctrl.chat.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Julius Alvarado on 2/17/2019.
+ */
+
+(function(){
+    'use strict';
+
+
+}());

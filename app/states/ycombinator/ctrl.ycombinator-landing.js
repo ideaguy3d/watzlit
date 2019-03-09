6a8795694002da0b383a63e761a3a52b7e8a7065
Julius Alvarado <javascript.uiux@gmail.com>
2019-03-09 03:57:38 -0800

    starting to implement a grid

--- app/states/ycombinator/ctrl.ycombinator-landing.js
+++ app/states/ycombinator/ctrl.ycombinator-landing.js
@@ -2,14 +2,12 @@
  * Created by Julius Alvarado on 3/4/2019.
  */
 
-(function(){
+(function () {
     'use strict';
 
-    angular.module('edhubJobsApp').controller('YCombinatorCtrl', [
-        YCombinatorCtrlClass
-    ]);
+    angular.module('edhubJobsApp').controller('YCombinatorCtrl', [YCombinatorCtrlClass]);
 
-    function YCombinatorCtrlClass () {
+    function YCombinatorCtrlClass() {
         var vm = this;
         vm.ycombinatorMessage = "Talent Opportunities at Y Combinator";
     }

5134e5268554948a214ca179b8a8ae2117374203
Julius Alvarado <javascript.uiux@gmail.com>
2019-05-10 09:14:31 -0700

    DEPLOY - using real data for clubs in sf and info box links to a
    generice club profile view that we will wire up to a db soon

--- app/states/club-profile/ctrl.club-profile.js
+++ app/states/club-profile/ctrl.club-profile.js
@@ -1,21 +1,19 @@
+"use strict";
+
 /**
  * Created by Julius Alvarado on 5/9/2019.
  */
+(function () {
+  'use strict';
 
-(function(){
-    'use strict';
+  angular.module('edhubJobsApp').controller('ClubProfileCtrl', ['$rootScope', ClubProfileCtrlClass]);
 
-    angular.module('edhubJobsApp').controller('ClubProfileCtrl', [
-        '$rootScope', ClubProfileCtrlClass
-    ]);
-
-    function ClubProfileCtrlClass ($rootScope) {
-        let vm = this;
-        vm.info = 'ClubProfileCtrl wired up to view ^_^';
-        vm.bgImage = {
-            'background-position': 'center center',
-            'background-image': 'url(images/club.jpg)'
-        }
-    }
-
-}());
+  function ClubProfileCtrlClass($rootScope) {
+    var vm = this;
+    vm.info = 'ClubProfileCtrl wired up to view ^_^';
+    vm.bgImage = {
+      'background-position': 'center center',
+      'background-image': 'url(images/club.jpg)'
+    };
+  }
+})();
--- app/states/learning-management/ctrl.learning-management.js
+++ app/states/learning-management/ctrl.learning-management.js
@@ -1,17 +1,15 @@
+"use strict";
+
 /**
  * Created by Julius Alvarado on 4/4/2019.
  */
+(function () {
+  'use strict';
 
-(function(){
-    'use strict';
+  function LearningManagementClass() {
+    var vm = this;
+    vm.message = 'Create courses or quick tutorials.';
+  }
 
-    function LearningManagementClass () {
-        var vm = this;
-        vm.message = 'Create courses or quick tutorials.'
-    }
-
-    angular.module('edhubJobsApp')
-        .controller('LearningManagementCtrl', [
-            LearningManagementClass
-        ]);
-}());
+  angular.module('edhubJobsApp').controller('LearningManagementCtrl', [LearningManagementClass]);
+})();
